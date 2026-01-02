9461a477dde2da6caa9e092630e77fbf51e69883
unknown <adarshkamble01@gmail.com>
2026-01-02 17:28:42 +0530

    minor fixes

--- worker/dist/src/solana.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendSolana = void 0;
-function sendSolana(publicId, amount) { }
-exports.sendSolana = sendSolana;
